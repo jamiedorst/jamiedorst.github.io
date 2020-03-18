dd59432e1cfca538488e7a5cd45687c8b229f9d5
Jamie Dorst <Jamie@Jamie.local>
2020-03-18 07:49:15 -0700

    fixed outside routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,17 +4,19 @@ import Landing from "./landing";
 import CourseAdvisor from "./courseAdvisor";
 import About from "./about";
 // import Error from "./error";
-import { BrowserRouter as Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/CMUCourseAdvisor" component={CourseAdvisor} />
-        {/* <Route component={Error} /> */}
-      </Switch>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/CMUCourseAdvisor" component={CourseAdvisor} />
+          {/* <Route component={Error} /> */}
+        </Switch>
+      </Router>
     </>
   );
 }
